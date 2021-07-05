9147e995a413a05a40bb9990abb59088596844e2
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-07-05 22:59:36 +0100

    api routes added for the today tasks and also upcoming tsks

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Resources\UpcomingTaskResource;
+use App\Models\Today;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Upcoming;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,42 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// ** Upcoming Tasks *//
+Route::get("/upcoming", function(){
+    $upcoming=Upcoming::all();
+
+    return UpcomingTaskResource::collection($upcoming);
+});
+//Add A new TASK 
+Route::post('/Upcoming', function( Request  $request){
+    return Upcoming::create([
+        'title'=>$request->title,
+        'taskId'=>$request->taskId,
+        'waiting'=>$request->waiting,
+
+    ]);
+});
+#delete upcoming request 
+Route::delete('/upcomming/{taskId}',function($taskId){
+ DB::table('upcomings')->where('taskId', $taskId)->delete();
+
+ return 204;
+} );
+
+//* Today task */
+Route::post('/dailytask', function(Request $request){
+    return Today::create([
+        'id'=>$request->id,
+        'title'=>$request->title,
+        'taskId'=> $request->taskId
+    ]);
+});
+
+//Delete Today Task */
+
+Route::delete('dailytask/{taskId}', function($taskId){
+ DB::table('todays')->where('taskId',$taskId)->delete();
+ return 204;
 });
