689a00e55fcb58480756426b0110f93d645c0912
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-07-05 16:53:54 +0100

    Project setup , database created , models and migration files created

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
+        
+
+      
+    </head>
+    <body class="antialiased">
+        <div id="app">
+           ffggfgfg 
+        </div>
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
